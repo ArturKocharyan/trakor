4cfba7a36029e2a3a7f4682567c3135567f5f5fe
ArturKocharyan <a.kocharyan1997@gmail.com>
2024-04-10 18:22:07 +0400

    texts bug fix

--- src/Components/TeamProgress/TeamProgress.jsx
+++ src/Components/TeamProgress/TeamProgress.jsx
@@ -11,13 +11,13 @@ function TeamProgress() {
                 <span className={style.heading}>Track your team progress</span>
                 <div className={style.description_container} >
                     <span className={style.description} >
-                        <p>Ut posuere felis arcu tellus tempus in in ultricies.</p> <p>Gravida id nibh ornare viverra.</p>
-                        <p>Ultrices faucibus neque velit risus ac id lorem.</p>
+                        <p>Ut posuere felis arcu tellus tempus in in ultricies.Gravida id nibh ornare viverra.
+                            Ultrices faucibus neque velit risus ac id lorem.</p>
                     </span>
                 </div>
                 <div className={style.buttons_container} >
-                    <CustomButton  backgroundColor="#4F46E5" textColor="white" border='#6366F1' maxWidth='256px' children='Start for free' />
-                    <CustomButton  backgroundColor="#334155" textColor="white" border='#475569' maxWidth='256px' children='Request more info' />
+                    <CustomButton backgroundColor="#4F46E5" textColor="white" border='#6366F1' maxWidth='256px' children='Start for free' />
+                    <CustomButton backgroundColor="#334155" textColor="white" border='#475569' maxWidth='256px' children='Request more info' />
                 </div>
                 <div className={style.image_container} >
                     <img src={img} alt='shape' ></img>
